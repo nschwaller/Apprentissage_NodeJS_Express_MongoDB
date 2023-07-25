9a1addfa1a2dc55d56353627277e1b242028d816
Nadia <nschwaller@tiz.fr>
2023-07-25 09:57:14 +0200

    Traité formulaire/ donnée post

--- indes.js
+++ indes.js
@@ -7,6 +7,10 @@ const port = 3000;
 // On définis le dossier public comme dossier static
 app.use(express.static('public'));
 
+// ces deux use permettent de traité des formulaires
+app.use(express.json()) 
+app.use(express.urlencoded({ extended: true })) 
+
 // si on fait un get sans envoyer de réponse (res) alors ca tournera en boucle
 // Ici on envoie une réponse au chemin par defaut (localhost:3000/)
 app.get('/', (req, res) => {
@@ -21,6 +25,16 @@ app.get('/bonjour', (req, res) => {
 // On envoie une réponse qui correspond a notre fichier html page.html
 app.get('/fichier/html', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/page.html'));
+});
+
+app.post('/form', (req, res) => {
+    console.log(req.body);
+
+    if(req.body.password == '1234'){
+        res.send('Formulaire traité!');
+    }else{
+        res.redirect('/fichier/html?mdpIncorrect=1');
+    }
 });
 
 // On récupère le nom dans la route et on utilis req.params qui est un tableau contenant tout nos paramètres
